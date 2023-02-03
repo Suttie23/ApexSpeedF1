29a50ac8644d2e4bff6b74c1159036987c93d16e
Suttie23 <76448183+Suttie23@users.noreply.github.com>
2023-02-03 13:14:32 +0000

    Initial Commit
    First commit, the system is currently a simple console app that listens to and parses the UDP for F1 2021 and determines the UDP format.

---
+++ F1_21_Telemetry/F1_21_Telemetry/PacketHeader.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F1_21_Telemetry
+{
+
+    // UDP Structure for the PacketHeader
+    struct PacketHeader
+    {
+        public ushort packetFormat;
+        public byte gameMajorVersion;
+        public byte gameMinorVersion;
+        public byte packetVersion;
+        public byte packetId;
+        public ulong sessionUID;
+        public float sessionTime;
+        public uint frameIdentifier;
+        public byte playerCarIndex;
+        public byte secondaryPlayerCarIndex;
+
+        // Convert to appropriate type
+        public static PacketHeader FromByteArray(byte[] data)
+        {
+            var packetHeader = new PacketHeader
+            {
+                packetFormat = BitConverter.ToUInt16(data, 0),
+                gameMajorVersion = data[2],
+                gameMinorVersion = data[3],
+                packetVersion = data[4],
+                packetId = data[5],
+                sessionUID = BitConverter.ToUInt64(data, 6),
+                sessionTime = BitConverter.ToSingle(data, 14),
+                frameIdentifier = BitConverter.ToUInt32(data, 18),
+                playerCarIndex = data[22],
+                secondaryPlayerCarIndex = data[23]
+            };
+
+            return packetHeader;
+        }
+    }
+
+}
---
+++ F1_21_Telemetry/F1_21_Telemetry/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Globalization;
+using System.IO.Ports;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Timers;
+
+namespace F1_21_Telemetry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Ensuring floats use dots as decimal separator
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+            // Setting up UDP Listener
+            UdpClient receivingUdpClient = new(20777);
+            IPEndPoint RemoteIpEndPoint = new(IPAddress.Any, 0);
+            Console.WriteLine("Listening to F1 Session...\n");
+
+
+            while (true)
+            {
+                // If Receiving
+                if (receivingUdpClient.Available > 0)
+                {
+                    byte[] receiveBytes = receivingUdpClient.Receive(ref RemoteIpEndPoint);
+
+                    // Receive UDP bytes from the PacketHeader
+                    var packetHeader = PacketHeader.FromByteArray(receiveBytes);
+
+                    // Determine game version and exit with error if not 2021
+                    if (packetHeader.packetFormat != 2021)
+                    {
+                        ExitWithError($"UDP format {packetHeader.packetFormat} not supported. Current version only supports 2021!");
+                    }
+                }
+                else
+                {
+                    Thread.Sleep(50);
+                }
+            }
+
+        }
+
+        // Exit with error message
+        static void ExitWithError(string error)
+        {
+            Console.WriteLine(error);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+            Environment.Exit(0);
+        }
+    }
+}
