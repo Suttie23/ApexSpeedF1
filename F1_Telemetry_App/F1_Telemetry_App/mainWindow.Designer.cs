f3407399bb5bea8f1e573658b0e8b918c6b2711b
Suttie23 <76448183+Suttie23@users.noreply.github.com>
2023-02-06 16:44:40 +0000

    Switched to GUI
    Moved from console app to WinForms. Application now runs the UDP listener Asynchronously with the GUI and displays the UDP format version as well as session time.

---
+++ F1_Telemetry_App/F1_Telemetry_App/mainWindow.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace F1_Telemetry_App
+{
+    partial class mainWindow
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            ListenerButton = new Button();
+            StatusBox = new RichTextBox();
+            VerLabel = new Label();
+            SessionTime = new Label();
+            SuspendLayout();
+            // 
+            // ListenerButton
+            // 
+            ListenerButton.Location = new Point(226, 543);
+            ListenerButton.Name = "ListenerButton";
+            ListenerButton.Size = new Size(75, 41);
+            ListenerButton.TabIndex = 0;
+            ListenerButton.Text = "Start Session";
+            ListenerButton.UseVisualStyleBackColor = true;
+            ListenerButton.Click += ListenerButton_Click;
+            // 
+            // StatusBox
+            // 
+            StatusBox.Location = new Point(12, 381);
+            StatusBox.Name = "StatusBox";
+            StatusBox.Size = new Size(501, 156);
+            StatusBox.TabIndex = 1;
+            StatusBox.Text = "";
+            // 
+            // VerLabel
+            // 
+            VerLabel.AutoSize = true;
+            VerLabel.Location = new Point(12, 9);
+            VerLabel.Name = "VerLabel";
+            VerLabel.Size = new Size(77, 15);
+            VerLabel.TabIndex = 2;
+            VerLabel.Text = "UDP Format: ";
+            // 
+            // SessionTime
+            // 
+            SessionTime.AutoSize = true;
+            SessionTime.Location = new Point(12, 35);
+            SessionTime.Name = "SessionTime";
+            SessionTime.Size = new Size(77, 15);
+            SessionTime.TabIndex = 4;
+            SessionTime.Text = "UDP Format: ";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(525, 596);
+            Controls.Add(SessionTime);
+            Controls.Add(VerLabel);
+            Controls.Add(StatusBox);
+            Controls.Add(ListenerButton);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button ListenerButton;
+        private RichTextBox StatusBox;
+        private Label VerLabel;
+        private Label SessionTime;
+    }
+}
---
+++ F1_Telemetry_App/F1_Telemetry_App/mainWindow.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Drawing.Text;
+using System.Globalization;
+using System.IO.Ports;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Timers;
+
+namespace F1_Telemetry_App
+{
+    public partial class mainWindow : Form
+    {
+
+        UdpClient receivingUdpClient = new(20777);
+
+        public mainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void ListenerButton_Click(object sender, EventArgs e)
+        {
+            // UDP Listener
+            try
+            {
+                // Begin asynchronous listening
+                receivingUdpClient.BeginReceive(new AsyncCallback(TelemetryReceiver), null);
+
+            }
+            catch (Exception ex)
+            {
+                // Display error
+                StatusBox.Text += ex.Message.ToString();
+
+            }
+
+        }
+
+        void TelemetryReceiver(IAsyncResult res)
+        {
+            // Remote host IP
+            IPEndPoint RemoteIpEndPoint = new(IPAddress.Any, 0);
+
+            // Return UDP datagram
+            byte[] receiveBytes = receivingUdpClient.Receive(ref RemoteIpEndPoint);
+
+            // Header UDP packet conversion
+            var packetHeader = PacketHeader.FromByteArray(receiveBytes);
+
+            // Invoke a delegate to ensure there is no cross-threading!
+            this.Invoke(new MethodInvoker(delegate
+            {
+                // If correct version
+                if (packetHeader.packetFormat == 2021)
+                {
+                    VerLabel.Text = "Running Supported Version: " + packetHeader.packetFormat;
+                    SessionTime.Text = packetHeader.sessionTime.ToString();
+                }
+                // If unsupported version
+                else
+                {
+                    MessageBox.Show("UDP Format Error!", "UDP Format " + packetHeader.packetFormat.ToString() + " is unsupported" + "\n\nPlease change UDP Format to 2021 (Settings > Telemetry Settings > UDP Format", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    VerLabel.Text = "Running Unsupported Version: " + packetHeader.packetFormat;
+                    // Need to close the UDP connection properly when there is a format mismatch
+                    //receivingUdpClient.Close();
+                }
+
+            }));
+
+            // Begin Call Async Method
+            receivingUdpClient.BeginReceive(new AsyncCallback(TelemetryReceiver), null);
+
+        }
+
+    }
+
+}
