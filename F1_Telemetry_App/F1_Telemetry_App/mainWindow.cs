2961945924b13327e3b19bbe2f8bab6dffc65dab
Suttie23 <76448183+Suttie23@users.noreply.github.com>
2023-02-16 12:28:56 +0000

    Added F1 2021 Nuget Package
    Removed my own attempts at writing my own UDP telemetry Parser and replaced it with one I discovered as a Nuget Package. While my version worked, it was far less efficient than this tried and tested one. As such, this will be used instead!
    
    Why reinvent the wheel when it's already here?

--- F1_Telemetry_App/F1_Telemetry_App/mainWindow.cs
+++ F1_Telemetry_App/F1_Telemetry_App/mainWindow.cs
@@ -13,6 +13,8 @@ using F1_Telemetry_App;
 using LiveChartsCore.Measure;
 using System.Security.Permissions;
 using LiveCharts.Defaults;
+using Codemasters.F1_2021;
+using Microsoft.VisualStudio.PlatformUI;
 
 namespace F1_Telemetry_App
 {
@@ -99,100 +101,123 @@ namespace F1_Telemetry_App
             // Return UDP datagram
             byte[] receiveBytes = receivingUdpClient.Receive(ref RemoteIpEndPoint);
 
-            // Header UDP packet conversion
-            var packetHeader = PacketHeader.FromByteArray(receiveBytes);
-
-            // If unsupported version
-            if (packetHeader.packetFormat != 2020)
+            try
             {
+
+                PacketType pt = CodemastersToolkit.GetPacketType(receiveBytes);
+
+                Packet pa = new Packet();
+                pa.LoadBytes(receiveBytes);
+
+                // Ensure the game is running the 2021 UDP format
+                if (pa.PacketFormat != 2021)
+                {
+                    this.Invoke(new MethodInvoker(delegate
+                    {
+                        MessageBox.Show("UDP Format is unsupported" + "\n\nPlease change UDP Format to 2021 (Settings > Telemetry Settings > UDP Format", "UDP Format Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                        // Exit the application if there is a version mismatch!
+                        this.Close();
+                        Environment.Exit(0);
+                    }));
+                }
+                else
+                {
+
+                    this.Invoke(new MethodInvoker(delegate
+                    {
+                        VerLabel.Text = "Running Supported Version: " + pa.PacketFormat.ToString();
+                        SessionTimeLabel.Text = pa.SessionTime.ToString();
+
+                    }));
+                }
+
+                // IF Car Telemetry Packet
+                if (pt == PacketType.CarTelemetry)
+                {
+                    TelemetryPacket telPack = new TelemetryPacket();
+                    telPack.LoadBytes(receiveBytes);
+
+                    this.Invoke(new MethodInvoker(delegate
+                    {
+
+                        ThrottleGauge.EasingFunction = null;
+                        ThrottleGauge.Total = 1;
+                        ThrottleGauge.InitialRotation = -225;
+                        ThrottleGauge.MaxAngle = 270;
+                        ThrottleGauge.Series = new GaugeBuilder()
+                            .WithLabelsSize(25)
+                            .WithInnerRadius(50)
+                            .WithBackgroundInnerRadius(50)
+                            .AddValue(Math.Round(telPack.FieldTelemetryData[telPack.PlayerCarIndex].Throttle, 2))
+                            .BuildSeries();
+
+                        SpeedGauge.EasingFunction = null;
+                        SpeedGauge.Total = 1;
+                        SpeedGauge.InitialRotation = -225;
+                        SpeedGauge.MaxAngle = 270;
+                        SpeedGauge.Series = new GaugeBuilder()
+                            .WithLabelsSize(25)
+                            .WithInnerRadius(50)
+                            .WithBackgroundInnerRadius(50)
+                            .AddValue(telPack.FieldTelemetryData[telPack.PlayerCarIndex].SpeedMph)
+                            .BuildSeries();
+
+                        BrakeGauge.EasingFunction = null;
+                        BrakeGauge.Total = 1;
+                        BrakeGauge.InitialRotation = -225;
+                        BrakeGauge.MaxAngle = 270;
+                        BrakeGauge.Series = new GaugeBuilder()
+                            .WithLabelsSize(25)
+                            .WithInnerRadius(50)
+                            .WithBackgroundInnerRadius(50)
+                            .AddValue(Math.Round(telPack.FieldTelemetryData[telPack.PlayerCarIndex].Brake, 2))
+                            .BuildSeries();
+
+                    }));
+
+                }
+
+                // IF Car CarStatus Packet
+                if (pt == PacketType.CarStatus)
+                {
+                    CarStatusPacket statusPack = new CarStatusPacket();
+                    statusPack.LoadBytes(receiveBytes);
+
+                    this.Invoke(new MethodInvoker(delegate
+                    {
+
+                        ERSGauge.EasingFunction = null;
+                        ERSGauge.Total = 1;
+                        ERSGauge.InitialRotation = -225;
+                        ERSGauge.MaxAngle = 270;
+                        ERSGauge.Series = new GaugeBuilder()
+                            .WithLabelsSize(25)
+                            .WithInnerRadius(50)
+                            .WithBackgroundInnerRadius(50)
+                            .AddValue(statusPack.FieldCarStatusData[statusPack.PlayerCarIndex].ErsStoredEnergyJoules)
+                            .BuildSeries();
+
+                        TyreCompundLabel.Text = statusPack.FieldCarStatusData[statusPack.PlayerCarIndex].EquippedTyreCompound.ToString();
+                        ERSModeLabel.Text = statusPack.FieldCarStatusData[statusPack.PlayerCarIndex].SelectedErsDeployMode.ToString();
+
+                    }));
+
+                }
+
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+
                 this.Invoke(new MethodInvoker(delegate
                 {
-                    MessageBox.Show("UDP Format " + packetHeader.packetFormat.ToString() + " is unsupported" + "\n\nPlease change UDP Format to 2021 (Settings > Telemetry Settings > UDP Format", "UDP Format Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    VerLabel.Text = "Running Unsupported Version: " + packetHeader.packetFormat;
+                    MessageBox.Show("UDP Format is unsupported" + "\n\nPlease change UDP Format to 2021 (Settings > Telemetry Settings > UDP Format", "UDP Format Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
 
                     // Exit the application if there is a version mismatch!
                     this.Close();
                     Environment.Exit(0);
-                }));
-
-            }
-            // If supported version
-            else
-            {
-                this.Invoke(new MethodInvoker(delegate
-                {
-                    VerLabel.Text = "Running Supported Version: " + packetHeader.packetFormat;
-                    SessionTimeLabel.Text = packetHeader.sessionTime.ToString();
-
-                }));
-            }
-
-            if (packetHeader.packetId == 2) // Lap Data
-            {
-
-            }
-            else if (packetHeader.packetId == 6) // Car Telemetry
-            {
-
-                var packet = PacketCarTelemetryData.FromByteArray(receiveBytes);
-
-                this.Invoke(new MethodInvoker(delegate
-                {
-
-                    ThrottleGauge.EasingFunction = null;
-                    ThrottleGauge.Total = 1;
-                    ThrottleGauge.InitialRotation = -225;
-                    ThrottleGauge.MaxAngle = 270;
-                    ThrottleGauge.Series = new GaugeBuilder()
-                        .WithLabelsSize(25)
-                        .WithInnerRadius(50)
-                        .WithBackgroundInnerRadius(50)
-                        .AddValue(Math.Round(packet.carTelemetryData[packetHeader.playerCarIndex].throttle, 2))
-                        .BuildSeries();
-
-                    SpeedGauge.EasingFunction = null;
-                    SpeedGauge.Series = new GaugeBuilder()
-                        .WithLabelsSize(25)
-                        .WithInnerRadius(50)
-                        .WithOffsetRadius(10)
-                        .WithBackgroundInnerRadius(50)
-                        .AddValue(Math.Round(packet.carTelemetryData[packetHeader.playerCarIndex].speed * 0.621371192))
-                        .BuildSeries();
-
-                    BrakeGauge.EasingFunction = null;
-                    BrakeGauge.Total = 1;
-                    BrakeGauge.InitialRotation = -225;
-                    BrakeGauge.MaxAngle = 270;
-                    BrakeGauge.Series = new GaugeBuilder()
-                        .WithLabelsSize(25)
-                        .WithInnerRadius(50)
-                        .WithBackgroundInnerRadius(50)
-                        .AddValue(Math.Round(packet.carTelemetryData[packetHeader.playerCarIndex].brake, 2))
-                        .BuildSeries();
-
-                }));
-
-            }
-            else if (packetHeader.packetId == 7) //Car status
-            {
-
-                var packet = PacketCarStatusData.FromByteArray(receiveBytes);
-
-                this.Invoke(new MethodInvoker(delegate
-                {
-                    TyreCompundLabel.Text = "Tyre Compound: " + packet.carStatusData[packetHeader.playerCarIndex].actualTyreCompound;
-                    ERSModeLabel.Text = "ERS Deployment Mode: " + packet.carStatusData[packetHeader.playerCarIndex].ersDeployMode;
-                    ERSStorageLabel.Text = "ERS Stored: " + packet.carStatusData[packetHeader.playerCarIndex].ersStoreEnergy + " Joules";
-
-                    ERSGauge.EasingFunction = null;
-                    ERSGauge.Series = new GaugeBuilder()
-                        .WithLabelsSize(15)
-                        .WithInnerRadius(50)
-                        .WithOffsetRadius(10)
-                        .WithBackgroundInnerRadius(50)
-                        .AddValue(packet.carStatusData[packetHeader.playerCarIndex].ersStoreEnergy)
-                        .BuildSeries();
-
                 }));
 
             }
