9b406ceaeebff7bcdbfd7fe3b1e3c167ab0159de
Suttie23 <76448183+Suttie23@users.noreply.github.com>
2023-02-18 15:06:47 +0000

    Moved to WPF
    Moved application from Winforms to WPF to provide a better UI and user experience. Was aiming to use Blazor, but due to time restrictions, getting to grips with a new markup would be unrealistic.

---
+++ ApexSpeedApp/ApexSpeedApp/MVVM/View/LiveAnalysisView.xaml.cs
@@ -0,0 +1,118 @@
+﻿using Codemasters.F1_2021;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ApexSpeedApp.MVVM.View
+{
+    /// <summary>
+    /// Interaction logic for LiveAnalysisView.xaml
+    /// </summary>
+    public partial class LiveAnalysisView : UserControl
+    {
+
+        UdpClient receivingUdpClient = new(20777);
+
+        public LiveAnalysisView()
+        {
+            InitializeComponent();
+        }
+
+        private void UDPListenerButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            // UDP Listener
+            try
+            {
+                // Begin asynchronous listening
+                receivingUdpClient.BeginReceive(new AsyncCallback(TelemetryReceiver), null);
+                ListeningLabel.Content = "Listening for UDP Data...";
+
+            }
+            catch (Exception ex)
+            {
+                // Display error
+                //DebugBox.Text += ex.Message.ToString();
+
+            }
+
+            void TelemetryReceiver(IAsyncResult res)
+            {
+                // Remote host IP
+                IPEndPoint RemoteIpEndPoint = new(IPAddress.Any, 0);
+
+                // Return UDP datagram
+                byte[] receiveBytes = receivingUdpClient.Receive(ref RemoteIpEndPoint);
+
+                try
+                {
+
+                    PacketType pt = CodemastersToolkit.GetPacketType(receiveBytes);
+
+                    Packet pa = new Packet();
+                    pa.LoadBytes(receiveBytes);
+
+                    // Ensure the game is running the 2021 UDP format
+                    if (pa.PacketFormat == 2021)
+                    {
+
+                        Dispatcher.BeginInvoke(new Action(delegate
+                        {
+
+                            ListenerTestLabel.Content = "I work on WPF too, I am running the: " + pa.PacketFormat.ToString() + " UDP Format!";
+
+                        }));
+                    }
+
+                    // IF Car Telemetry Packet
+                    if (pt == PacketType.CarTelemetry)
+                    {
+                        TelemetryPacket telPack = new TelemetryPacket();
+                        telPack.LoadBytes(receiveBytes);
+
+
+                    }
+
+                    // IF Car CarStatus Packet
+                    if (pt == PacketType.CarStatus)
+                    {
+                        CarStatusPacket statusPack = new CarStatusPacket();
+                        statusPack.LoadBytes(receiveBytes);
+
+                    }
+
+                }
+                catch (Exception e)
+                {
+
+                    MessageBox.Show("Error Message: " + e.Message + "\n\nIt is likely that your  UDP Format is not set to to 2021, change this by going to (Settings > Telemetry Settings > UDP Format > 2021) in your F1 game!", "UDP Format Error!");
+
+                    // Exit the application if there is a version mismatch!
+                    Dispatcher.BeginInvoke(new Action(delegate
+                    {
+
+                        System.Windows.Application.Current.Shutdown();
+
+                    }));
+                }
+
+                // Begin Call Async Method
+                receivingUdpClient.BeginReceive(new AsyncCallback(TelemetryReceiver), null);
+
+            }
+        }
+    }
+}
---
+++ ApexSpeedApp/ApexSpeedApp/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,55 @@
+﻿using ApexSpeedApp.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApexSpeedApp.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand LiveAnalysisViewCommand { get; set; }
+
+        public HomeViewModel HomeVM { get; set; }
+        public LiveAnalysisViewModel LiveAnalysisVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+
+            set 
+            { 
+                _currentView = value; 
+                OnPropertyChanged();
+            }
+        }
+
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            LiveAnalysisVM = new LiveAnalysisViewModel();
+
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+
+            LiveAnalysisViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = LiveAnalysisVM;
+            });
+
+
+        }
+
+
+    }
+}
