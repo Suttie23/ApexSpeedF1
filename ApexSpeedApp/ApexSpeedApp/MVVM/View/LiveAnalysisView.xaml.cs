4265ab0ccc513b0f98af1e6e63b2184c4a1a8867
Suttie23 <76448183+Suttie23@users.noreply.github.com>
2023-02-19 01:52:28 +0000

    Minor Changes
    Added Spanner image and done some cleanup

--- ApexSpeedApp/ApexSpeedApp/MVVM/View/LiveAnalysisView.xaml.cs
+++ ApexSpeedApp/ApexSpeedApp/MVVM/View/LiveAnalysisView.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Security.Cryptography.Pkcs;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -15,16 +16,19 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ApexSpeedApp.MVVM;
+using ApexSpeedApp.MVVM.ViewModel;
 
 namespace ApexSpeedApp.MVVM.View
 {
     /// <summary>
     /// Interaction logic for LiveAnalysisView.xaml
     /// </summary>
+    /// 
     public partial class LiveAnalysisView : UserControl
     {
 
-        UdpClient receivingUdpClient = new(20777);
+        private bool? isStreaming = false;
 
         public LiveAnalysisView()
         {
@@ -34,9 +38,12 @@ namespace ApexSpeedApp.MVVM.View
         private void UDPListenerButton_Click(object sender, RoutedEventArgs e)
         {
 
+            UdpClient receivingUdpClient = new(20777);
+
             // UDP Listener
             try
             {
+
                 // Begin asynchronous listening
                 receivingUdpClient.BeginReceive(new AsyncCallback(TelemetryReceiver), null);
                 ListeningLabel.Content = "Listening for UDP Data...";
@@ -46,6 +53,7 @@ namespace ApexSpeedApp.MVVM.View
             {
                 // Display error
                 //DebugBox.Text += ex.Message.ToString();
+                MessageBox.Show("Error Message: " + ex.Message);
 
             }
 
@@ -54,65 +62,72 @@ namespace ApexSpeedApp.MVVM.View
                 // Remote host IP
                 IPEndPoint RemoteIpEndPoint = new(IPAddress.Any, 0);
 
-                // Return UDP datagram
-                byte[] receiveBytes = receivingUdpClient.Receive(ref RemoteIpEndPoint);
+                    // Return UDP datagram
+                    byte[] receiveBytes = receivingUdpClient.Receive(ref RemoteIpEndPoint);
 
-                try
-                {
-
-                    PacketType pt = CodemastersToolkit.GetPacketType(receiveBytes);
-
-                    Packet pa = new Packet();
-                    pa.LoadBytes(receiveBytes);
-
-                    // Ensure the game is running the 2021 UDP format
-                    if (pa.PacketFormat == 2021)
+                    try
                     {
 
+                        PacketType pt = CodemastersToolkit.GetPacketType(receiveBytes);
+
+                        Packet pa = new Packet();
+                        pa.LoadBytes(receiveBytes);
+
+                        // Ensure the game is running the 2021 UDP format
+                        if (pa.PacketFormat == 2021)
+                        {
+
+                            Dispatcher.BeginInvoke(new Action(delegate
+                            {
+
+                                ListenerTestLabel.Content = "UDP Format: " + pa.PacketFormat.ToString() + " Detected";
+
+                            }));
+                        }
+
+                        // IF Car Telemetry Packet
+                        if (pt == PacketType.CarTelemetry)
+                        {
+                            TelemetryPacket telPack = new TelemetryPacket();
+                            telPack.LoadBytes(receiveBytes);
+
+                       
+                        }
+
+                        // IF Car CarStatus Packet
+                        if (pt == PacketType.CarStatus)
+                        {
+                            CarStatusPacket statusPack = new CarStatusPacket();
+                            statusPack.LoadBytes(receiveBytes);
+
+                        }
+
+                    }
+                    catch (Exception e)
+                    {
+
+                        MessageBox.Show("Error Message: " + e.Message + "\n\nIt is likely that your  UDP Format is not set to to 2021, change this by going to (Settings > Telemetry Settings > UDP Format > 2021) in your F1 game!", "UDP Format Error!");
+
+                        // Exit the application if there is a version mismatch!
                         Dispatcher.BeginInvoke(new Action(delegate
                         {
 
-                            ListenerTestLabel.Content = "I work on WPF too, I am running the: " + pa.PacketFormat.ToString() + " UDP Format!";
+                            System.Windows.Application.Current.Shutdown();
 
                         }));
                     }
 
-                    // IF Car Telemetry Packet
-                    if (pt == PacketType.CarTelemetry)
-                    {
-                        TelemetryPacket telPack = new TelemetryPacket();
-                        telPack.LoadBytes(receiveBytes);
-
-
-                    }
-
-                    // IF Car CarStatus Packet
-                    if (pt == PacketType.CarStatus)
-                    {
-                        CarStatusPacket statusPack = new CarStatusPacket();
-                        statusPack.LoadBytes(receiveBytes);
-
-                    }
-
-                }
-                catch (Exception e)
-                {
-
-                    MessageBox.Show("Error Message: " + e.Message + "\n\nIt is likely that your  UDP Format is not set to to 2021, change this by going to (Settings > Telemetry Settings > UDP Format > 2021) in your F1 game!", "UDP Format Error!");
-
-                    // Exit the application if there is a version mismatch!
-                    Dispatcher.BeginInvoke(new Action(delegate
-                    {
-
-                        System.Windows.Application.Current.Shutdown();
-
-                    }));
-                }
-
-                // Begin Call Async Method
-                receivingUdpClient.BeginReceive(new AsyncCallback(TelemetryReceiver), null);
-
+                    // Begin Call Async Method
+                    receivingUdpClient.BeginReceive(new AsyncCallback(TelemetryReceiver), null);
+                
             }
         }
+
+        private void UDPStopListenerButton_Click(object sender, RoutedEventArgs e)
+        {
+            //receivingUdpClient.Close();
+            ListeningLabel.Content = "No Longer Listening...";
+        }
+
     }
 }
