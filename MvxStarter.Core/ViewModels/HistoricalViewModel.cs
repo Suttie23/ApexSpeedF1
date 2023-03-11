e61bbf249516da2ca3e5530ce2f4ff9f31807253
Suttie23 <76448183+Suttie23@users.noreply.github.com>
2023-03-11 14:26:28 +0000

    Further Navigation Setup
    Added buttons to the Historical View that can Navigate to specific analysis types
    (Only navigation has been set up to ThrottleAnalysis thus far

--- MvxStarter.Core/ViewModels/HistoricalViewModel.cs
+++ MvxStarter.Core/ViewModels/HistoricalViewModel.cs
@@ -14,18 +14,24 @@ namespace ApexSpeed.Core.ViewModels
     {
         private readonly IMvxNavigationService _navigationService;
 
-        public IMvxCommand NavToHomeCommand => new MvxCommand(async () => await NavToHome());
 
+        public IMvxCommand NavToHomeCommand => new MvxCommand(async () => await NavToHome());
         public async Task NavToHome()
         {
             await _navigationService.Navigate<HomeViewModel>();
         }
 
-        public IMvxCommand NavToLiveTelemetryCommand => new MvxCommand(async () => await NavToLiveTelemetry());
 
+        public IMvxCommand NavToLiveTelemetryCommand => new MvxCommand(async () => await NavToLiveTelemetry());
         public async Task NavToLiveTelemetry()
         {
             await _navigationService.Navigate<TelemetryViewModel>();
+        }
+
+        public IMvxCommand NavToThrottleCommand => new MvxCommand(async () => await NavToThrottle());
+        public async Task NavToThrottle()
+        {
+            await _navigationService.Navigate<ThrottleAnalysisViewModel>();
         }
 
         public HistoricalViewModel(IMvxNavigationService navigationService)
